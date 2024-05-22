a39d925f77e902e23e3c518a9976493228c443fc
DevrajGohil20 <devrajgohil2003@gmail.com>
2024-05-22 09:27:47 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/leely/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.leely
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:leely/home_page.dart';
+import 'package:leely/pallete.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Leely',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
